a974f7626b8120ad79c5efcc56534fdce36b841b
Trần Vũ Phi Bằng <phibang7899@gmail.com>
2021-07-19 19:34:33 +0700

    Onl

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 // import ClassComponent from "./components/ClassComponent";
 // import FuntionsComponent from "./components/FuntionsComponent";
 // import HomeComponent from "./components/BaiTapComponent/HomeComponent";
-<<<<<<< HEAD
+
 import Databinding from './Databiding/Databinding'
-=======
-
-// import Databinding from './Databinding/Databinding'
-// import Demo from './Databinding/Demo';
-// import HandleEvent from "./HandleEvent/HandleEvent";
-// import StyleComponent from "./StyleComponent/StyleComponent";
-
-import StateDemo from "./StateDemo/StateDemo";
->>>>>>> Buoi1
 
 function App() {
   return (
@@ -23,7 +14,7 @@ function App() {
       {/* {<StyleComponent></StyleComponent>}
       <p>ahihi</p>
       <p className="fontBold">1213123</p> */}
-      {<StateDemo></StateDemo>}
+      {<Databinding></Databinding>}
     </div>
   );
 }
